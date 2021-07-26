eb7a3ab1366090809e3237a9a68a9168d42a7f04
Franco Naghetini <franconaghetini@gmail.com>
2021-07-26 15:32:23 -0300

    Update Table of Contents

--- variograms.jl
+++ variograms.jl
@@ -29,14 +29,14 @@ begin
 	gr(format=:png)
 end;
 
+# ╔═╡ 3bd915e1-2f58-451c-a0fb-8aec6d6f75d9
+PlutoUI.TableOfContents(aside=true, title="Sumário",
+						indent=true, depth=3)
+
 # ╔═╡ f8909bd5-9167-42ea-a302-a7a50bdc365c
 html"""
 <p style="background-color:lightgrey" xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><span property="dct:title">&nbsp&nbsp<b>Variogramas</b></span> por <span property="cc:attributionName">Franco Naghetini</span> é licenciado sob <a href="http://creativecommons.org/licenses/by/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"></a></p>
 """
-
-# ╔═╡ 3bd915e1-2f58-451c-a0fb-8aec6d6f75d9
-PlutoUI.TableOfContents(aside=true, title="Sumário",
-						indent=true, depth=2)
 
 # ╔═╡ 029c1951-054b-4f48-bc05-341250ce9f6a
 md" # Variogramas"
